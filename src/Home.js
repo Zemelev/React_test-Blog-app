f0d654e75eb7d22a102d17b5fdd191092a0674ac
Sava <zemelevsavelii@gmail.com>
2023-12-07 17:39:48 +0200

    React test Blog App || useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState('dan');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => { 
-        setName('nick');
-        setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{ name } is {age}</p>
-            <button onClick={(e) => handleClick('dan', e)}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
