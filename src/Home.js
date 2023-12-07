20670ced4f97ff3a8358014c353c0738ed989d3a
Sava <zemelevsavelii@gmail.com>
2023-12-08 00:31:02 +0200

    React test Blog App || Function as Props

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,20 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'My new website', body: 'lorem ipsum...', author: 'dan', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'dan')} title="Dan's blogs" /> */}
         </div>
      );
 }
