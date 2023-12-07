69013468723dcddb93c63ecd2e5856882a94de05
Sava <zemelevsavelii@gmail.com>
2023-12-07 16:49:12 +0200

    React test Blog App || useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('dan');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => { 
+        setName('nick');
+        setAge(30);
+    }
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <p>{ name } is {age}</p>
+            <button onClick={(e) => handleClick('dan', e)}>Click me</button>
         </div>
      );
 }
