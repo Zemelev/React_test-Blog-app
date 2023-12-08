43779014593b62166c8dcd8e8824d71613f8c785
Sava <zemelevsavelii@gmail.com>
2023-12-09 00:55:54 +0200

    React test Blog App || Error handling

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => { //fires on every render
@@ -11,18 +12,26 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data')
+                }
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
             })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.massage);
+            } )
         }, 1000);
     }, []); 
 
     return ( 
         <div className="home">
+            { error && <div>{error}</div>}
             { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'dan')} title="Dan's blogs" /> */}
