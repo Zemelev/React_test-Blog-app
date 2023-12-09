e2cde50737d9a96802cd5f02090476967d6a2636
Sava <zemelevsavelii@gmail.com>
2023-12-09 02:09:08 +0200

    React test Blog App || React Router v6.2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
 
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/create' element={<Create />} />
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
